2d48612ddca7c1b548a2c1e7eadd45dbe126b9c0
Fernando Ogata <fernandokogata@gmail.com>
2024-11-30 00:17:11 -0300

    chore: Alterando script de criação de DB ao inicializar docker. Adicionando pacote API ao laravel.

--- create-db.sql
+++ create-db.sql
@@ -1,1 +1,14 @@
-CREATE DATABASE backend;
+CREATE TABLE niveis
+(
+	id integer primary key generated always as identity,
+	nivel varchar(255) not null
+);
+CREATE TABLE desenvolvedores
+(
+  id integer primary key generated always as identity,
+  nivel_id integer REFERENCES niveis (id),
+  nome varchar(255),
+  sexo char(1),
+  data_nascimento date,
+  hobby varchar(255)
+);
